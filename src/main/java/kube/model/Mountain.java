0b767b2a51fd865391d87f50a53239a786870b79
Thomas Civade <thomas.civade@gmail.com>
2024-04-30 13:56:17 +0200

    starting moutain

--- src/main/java/kube/model/Mountain.java
+++ src/main/java/kube/model/Mountain.java
@@ -3,18 +3,19 @@ package kube.model;
 public class Mountain {
     private int[] m;
     private int base_size;
-    private static final int nbPerColor = 7;
+    private static final int nbBitPerColor = 7;
+
     Mountain(int size){
         base_size = size;
         m = new int[base_size];
     }
 
     public Color getCase(int x, int y){
-        return Color.getColor((m[x] >> ((y - 1) * nbPerColor)) & 127);
+        return Color.getColor((m[x] >> (y * nbBitPerColor)) & 127);
     }
 
     public void setCase(int x, int y, Color c){
-        m[x] = m[x] & (~127 << ((y-1 * nbPerColor)));
-        m[x] = m[x] & (c.getColorCode() << ((y-1 * nbPerColor)));
+        m[x] = m[x] & (~127 << ((y-1 * nbBitPerColor)));
+        m[x] = m[x] & (c.getColorCode() << (y * nbBitPerColor));
     }
 }
