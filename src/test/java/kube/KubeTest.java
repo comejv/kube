173bcfacc4c39342543711eb96cb232e12612b47
Clement <clement.corbalan@etu.univ-grenoble-alpes.fr>
2024-05-01 09:55:10 +0200

    KubeTest advance

--- src/test/java/kube/KubeTest.java
+++ src/test/java/kube/KubeTest.java
@@ -5,8 +5,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.lang.reflect.Array;
+import java.util.ArrayList;
 
+import kube.model.Color;
 import kube.model.Kube;
 
 
@@ -21,10 +22,44 @@ public class KubeTest {
         assertEquals(0, kube.getHistoric().getDone().size());
         assertEquals(0, kube.getHistoric().getUndone().size());
         assertEquals(0, kube.getHistoric().getFirstPlayer());
-        assertEquals(kube.getP1(), kube.getCurrentPlayer());
         assertEquals(1, kube.getPhase());
-        
-
     }
     
+    @Test
+    public void testBag() {
+        Kube kube = new Kube();
+        ArrayList<Color> bag = kube.getBag();
+        assertEquals(9, bag.size());
+        int[] colors = new int[8];
+        for (Color c : bag) {
+            switch (c) {
+                case WHITE:
+                    colors[1]++;
+                    break;
+                case NATURAL:
+                    colors[2]++;
+                    break;
+                case RED:
+                    colors[3]++;
+                    break;
+                case GREEN:
+                    colors[4]++;
+                    break;
+                case BLUE:
+                    colors[5]++;
+                    break;
+                case YELLOW:
+                    colors[6]++;
+                    break;
+                case BLACK:
+                    colors[7]++;
+                    break;
+                default:
+                    assertFalse(true);
+            }
+        }
+        for (int i = 1; i < 8; i++) {
+            assertTrue(colors[i] >= 9);
+        }
+    }
 }
