ffb31f2214971ec06e8193b0344a467d266918d2
Thomas Civade <thomas.civade@gmail.com>
2024-05-06 15:42:40 +0200

    random ia

--- src/main/java/kube/model/ai/RandomAI.java
+++ src/main/java/kube/model/ai/RandomAI.java
@@ -1,33 +1,49 @@
 package kube.model.ai;
 
-
 import java.util.ArrayList;
+import java.util.Random;
 
 import kube.model.Color;
 import kube.model.Kube;
-import kube.model.Mountain;
 import kube.model.Player;
 import kube.model.move.*;
 
 public class RandomAI implements AI {
     Kube k3;
     Player player;
+    Random r;
 
-    public RandomAI(Kube k, Player p){
+    public RandomAI(Kube k, Player p) {
         k3 = k;
         player = p;
+        r = new Random();
     }
 
     public void preparationPhase() {
         ArrayList<Color> colArr = new ArrayList<>(player.getAvalaibleToBuild().keySet());
+        for (int i = 0; i < player.getMountain().getBaseSize(); i++) {
+            for (int j = 0; j < i + 1; j++) {
+                int n = r.nextInt(colArr.size());
+                Color c =  colArr.get(n);
+                player.addToMountain(i, j, c);
+                if (!player.isAvailableToBuild(c)){
+                    colArr.remove(n);
+                }
+            }
+        }
     }
 
     public void gamePhase() {
-        throw new UnsupportedOperationException("Unimplemented method 'gamePhase'");
+        
     }
 
-    public Move nextMove() {
-        throw new UnsupportedOperationException("Unimplemented method 'nextMove'");
+    public Move nextMove() throws Exception {
+        ArrayList<Move> moves = k3.moveSet();
+        if (moves.size() == 0){
+            throw new Exception("Aucun coup jouable");
+        } else {
+            return moves.get(r.nextInt(moves.size()));
+        }
     }
-    
+
 }
