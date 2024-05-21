1f9995675c3470727e099f5904563e6ba1bb5307
Deleuze <75162059+Stonksmen@users.noreply.github.com>
2024-05-21 10:43:17 +0200

    Implemented first rule pannel. It doesn't rescale for now

--- src/main/java/kube/view/panels/Overlay.java
+++ src/main/java/kube/view/panels/Overlay.java
@@ -2,18 +2,13 @@ package kube.view.panels;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionListener;
 import java.awt.GridBagConstraints;
 
-import javax.swing.JButton;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
 
 import kube.configuration.Config;
-import kube.view.components.Buttons.RulesButton;
 
 public class Overlay extends JPanel{
 
@@ -26,27 +21,7 @@ public class Overlay extends JPanel{
         setBackground(new Color(0, 0 , 0 , 150));
         setVisible(false);
 
-        JLabel ruleTitle = new JLabel("RULES", SwingConstants.CENTER);
-        ruleTitle.setFont(new Font("Jomhuria", Font.BOLD, (int) (Config.getInitHeight() / 6)));
-
-        GridBagConstraints elemGBC = new GridBagConstraints();
-        elemGBC.gridx = 0;
-        elemGBC.gridy = 0;
-        elemGBC.fill = GridBagConstraints.BOTH;
-        elemGBC.gridwidth = GridBagConstraints.REMAINDER;
-        elemGBC.anchor = GridBagConstraints.CENTER;
-        elemGBC.weighty = .5;
-        elemGBC.weightx = .5;
-        add(ruleTitle, elemGBC);
-
-        JButton suivant = new RulesButton("suivant");
-        elemGBC = new GridBagConstraints();
-        elemGBC.gridx = 0;
-        elemGBC.gridy = 1;
-        elemGBC.anchor = GridBagConstraints.LAST_LINE_END;
-        elemGBC.weighty = .5;
-        elemGBC.weightx = .5;
-        add(suivant, elemGBC);
+        add(new RulesPanel());
 
         buttonListener = al;
     }
