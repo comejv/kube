69e37b07e264ef4e4cedbb50f50f1ae30c569dbb
NotRage <grousson.dylan@gmail.com>
2024-05-31 12:02:33 +0200

    refactored Main.java

--- src/main/java/kube/Main.java
+++ src/main/java/kube/Main.java
@@ -1,5 +1,6 @@
 package kube;
 
+// Import kube classes
 import kube.configuration.ResourceLoader;
 import kube.controller.graphical.GUIControllers;
 import kube.model.Game;
@@ -9,10 +10,17 @@ import kube.model.action.Queue;
 import kube.view.GUI;
 
 public class Main {
-    // TODO : refactor this class to make it more readable
+
+    /**
+     * Main method of the program
+     * 
+     * @param args the arguments
+     */
     public static void main(String[] args) {
+
         new ResourceLoader();
         Kube kube = new Kube();
+        
         Queue<Action> eventsToModel = new Queue<>();
         Queue<Action> eventsToView = new Queue<>();
         Queue<Action> eventsToNetwork = new Queue<>();
