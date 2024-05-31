3541d0f3e559cc6280fe1ddbee99ad70c6e93be7
Côme VINCENT <44554692+comejv@users.noreply.github.com>
2024-05-31 10:24:56 +0200

    remove useless condition

--- src/main/java/kube/controller/graphical/MenuController.java
+++ src/main/java/kube/controller/graphical/MenuController.java
@@ -86,7 +86,7 @@ public class MenuController implements ActionListener, MouseListener {
     public void mousePressed(MouseEvent e) {
         Object source = e.getSource();
         if (SwingUtilities.isLeftMouseButton(e)) {
-            if (source instanceof JButton || source instanceof ButtonIcon) {
+            if (source instanceof ButtonIcon) {
                 toView.add(new Action(ActionType.SET_BUTTON_PRESSED, source));
             }
         }
@@ -95,7 +95,7 @@ public class MenuController implements ActionListener, MouseListener {
     public void mouseReleased(MouseEvent e) {
         Object source = e.getSource();
         if (SwingUtilities.isLeftMouseButton(e)) {
-            if (source instanceof JButton || source instanceof ButtonIcon) {
+            if (source instanceof ButtonIcon) {
                 toView.add(new Action(ActionType.SET_BUTTON_RELEASED, source));
             }
         }
@@ -103,14 +103,14 @@ public class MenuController implements ActionListener, MouseListener {
 
     public void mouseEntered(MouseEvent e) {
         Object source = e.getSource();
-        if (source instanceof JButton || source instanceof ButtonIcon) {
+        if (source instanceof ButtonIcon) {
             toView.add(new Action(ActionType.SET_BUTTON_HOVERED, source));
         }
     }
 
     public void mouseExited(MouseEvent e) {
         Object source = e.getSource();
-        if (source instanceof JButton || source instanceof ButtonIcon) {
+        if (source instanceof ButtonIcon) {
             toView.add(new Action(ActionType.SET_BUTTON_DEFAULT, source));
         }
     }
