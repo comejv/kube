5c94533bc10c1701a652d9fbebf7e66e9e466708
NotRage <grousson.dylan@gmail.com>
2024-05-31 12:22:48 +0200

    refactored NetworkListener.java and NetworkSender.java

--- src/main/java/kube/controller/network/NetworkListener.java
+++ src/main/java/kube/controller/network/NetworkListener.java
@@ -1,22 +1,43 @@
 package kube.controller.network;
 
-import java.io.IOException;
-
+// Import kube classes
 import kube.model.action.Action;
 import kube.model.action.ActionType;
 import kube.model.action.Queue;
 import kube.services.Network;
 
+// Import Java class
+import java.io.IOException;
+
 public class NetworkListener implements Runnable {
+
+    /**********
+     * ATTRIBUTES
+     **********/
 
     Network network;
     Queue<Action> networkToModel;
 
+    /**********
+     * CONSTRUCTOR
+     **********/
+
+    /**
+     * Constructor of the class
+     * 
+     * @param network the network object
+     * @param networkToModel the queue of actions to send to the model
+     */
     public NetworkListener(Network network, Queue<Action> networkToModel) {
         this.network = network;
         this.networkToModel = networkToModel;
     }
 
+    /**********
+     * METHOD
+     **********/
+
+     @Override
     public void run() {
         while (true) {
             try {
