64a7870ddafeb6aca9349dd5af19a98cd5255d2e
Côme VINCENT <44554692+comejv@users.noreply.github.com>
2024-06-01 18:51:05 +0200

    remove blank line

--- src/main/java/kube/view/GUI.java
+++ src/main/java/kube/view/GUI.java
@@ -101,7 +101,6 @@ public class GUI extends Thread {
 
         // After repaint start loading next panel
         createGlassPane();
-        
     }
 
     public void showPanel(String panelName) {
