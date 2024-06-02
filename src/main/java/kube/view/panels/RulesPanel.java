350a5c692a3f4d28877c74d7aaf86beafa181240
Deleuze <75162059+Stonksmen@users.noreply.github.com>
2024-06-02 18:29:49 +0200

    fix RulesPanel comporting like a kangaroo

--- src/main/java/kube/view/panels/RulesPanel.java
+++ src/main/java/kube/view/panels/RulesPanel.java
@@ -24,28 +24,32 @@ import kube.view.components.Buttons.RulesButton;
 public class RulesPanel extends JPanel {
     // TODO : refactor this class to make it more readables
 
-    private GUI gui;
     private int width;
     private int height;
     private MenuController buttonListener;
     private JPanel cardPanel;
-    private RulePanel rulePanel;
-    private static int totalRuleNb = 8;
+    private JTextArea[] textAreas;
+    private RulePanel[] rulePanels;
+    private int currentRuleNb;
+    private final int TOTAL_RULE_NB = 8;
+    private final Color BACKGROUND = GUIColors.ACCENT.toColor();
+    private final Color FOREGROUND = GUIColors.TEXT.toColor();
 
     public RulesPanel(GUI gui, MenuController buttonListener) {
 
-        this.gui = gui;
         this.buttonListener = buttonListener;
         width = Config.INIT_WIDTH / 2;
         height = Config.INIT_HEIGHT / 2;
 
-        width = Math.round(Config.INIT_WIDTH / 1.75f);
-        height = Math.round(Config.INIT_HEIGHT / 1.5f);
+        width = Math.round(Config.INIT_WIDTH / 1.5f);
+        height = Math.round(Config.INIT_HEIGHT / 1.25f);
 
         setLayout(new GridBagLayout());
         setPreferredSize(new Dimension(width, height));
         setBackground(GUIColors.ACCENT.toColor());
 
+        setCurrentRuleNb(1);
+        
         JLabel ruleTitle = new JLabel("REGLES", SwingConstants.CENTER);
         ruleTitle.setFont(new Font("Jomhuria", Font.BOLD, (int) (Config.INIT_HEIGHT / 6)));
         ruleTitle.setForeground(GUIColors.TEXT.toColor());
@@ -56,7 +60,7 @@ public class RulesPanel extends JPanel {
         elemGBC.weightx = 0.5;
         elemGBC.weighty = 0.1;
         add(ruleTitle, elemGBC);
-
+        
         cardPanel = new JPanel(new CardLayout());
         cardPanel.setPreferredSize(new Dimension(width, height));
         elemGBC = new GridBagConstraints();
@@ -66,62 +70,94 @@ public class RulesPanel extends JPanel {
         elemGBC.weighty = 0.9;
         elemGBC.fill = GridBagConstraints.BOTH;
         add(cardPanel, elemGBC);
-
-        rulePanel = new RulePanel();
-        cardPanel.add(rulePanel, "rule");
+        
+        textAreas = new JTextArea[TOTAL_RULE_NB];
+        rulePanels = new RulePanel[TOTAL_RULE_NB];
+        loadAllPanels();
 
         setVisible(true);
     }
-
-    public void nextRule() {
-        rulePanel.setRuleNb(rulePanel.getRuleNb() % totalRuleNb + 1);
-        rulePanel.ruleToShow();
-        rulePanel.revalidate();
-        rulePanel.repaint();
-    }
-
-    public void previousRule() {
-        rulePanel.setRuleNb(rulePanel.getRuleNb() - 1);
-        rulePanel.ruleToShow();
-        rulePanel.revalidate();
-        rulePanel.repaint();
-    }
-
-    private class RulePanel extends JPanel {
-        private int ruleNb = 1;
-        private Color background;
-        private Color foreground;
-        private JTextArea textArea;
-
-        private RulePanel() {
-            setLayout(new GridBagLayout());
-            background = GUIColors.ACCENT.toColor();
-            foreground = GUIColors.TEXT.toColor();
-            setBackground(background);
-            ruleToShow();
-        }
-
-        private void addTextArea(String name) {
-            textArea = new JTextArea(ResourceLoader.getText(name));
+    
+    private void loadAllTexts(){
+        for (int i = 0; i < TOTAL_RULE_NB; i++) {
+            Config.debug("load rule nb " + i);
+            //rules are numeroted from 1 to 8, not 0 to 7 thus the +1
+            JTextArea textArea = new JTextArea(ResourceLoader.getText("rule" + (i + 1)));
             textArea.setEditable(false);
             textArea.setWrapStyleWord(true);
             textArea.setLineWrap(true);
             textArea.setFont(new Font("Jomhuria", Font.BOLD, (int) (Config.INIT_HEIGHT / 14)));
-            textArea.setBackground(background);
-            textArea.setForeground(foreground);
+            textArea.setBackground(BACKGROUND);
+            textArea.setForeground(FOREGROUND);
             textArea.setOpaque(false);
             textArea.setBorder(null);
+            textAreas[i] = textArea;
+        }
+    }
 
+    private void loadAllPanels(){
+        loadAllTexts();
+        RulePanel rulePanel = new RulePanel();
+        rulePanel.addTextArea(0);
+        rulePanel.addNextButton(0);
+        rulePanel.addPreviousButton(0);
+        rulePanels[0] = rulePanel;
+        cardPanel.add(rulePanels[0]);
+        for (int i = 1; i < TOTAL_RULE_NB; i++) {
+             rulePanel = new RulePanel();
+            rulePanel.addTextArea(i);
+            rulePanel.addNextButton(i);
+            rulePanel.addPreviousButton(i);
+            rulePanel.setVisible(false);
+            rulePanels[i] = rulePanel;
+            cardPanel.add(rulePanels[i]);
+            rulePanel.setVisible(true);
+        }
+    }
+    
+    public void nextRule() {
+        cardPanel.removeAll();
+        setCurrentRuleNb(getCurrentRuleNb() + 1);
+        cardPanel.add(rulePanels[getCurrentRuleNb()]);
+        cardPanel.revalidate();
+        cardPanel.repaint();
+    }
+    
+    public void previousRule() {
+        cardPanel.removeAll();
+        setCurrentRuleNb(getCurrentRuleNb() - 1);
+        cardPanel.add(rulePanels[getCurrentRuleNb()]);
+        cardPanel.revalidate();
+        cardPanel.repaint();
+    }
+    
+    public int getCurrentRuleNb() {
+        return currentRuleNb;
+    }
+
+    public void setCurrentRuleNb(int i) {
+        currentRuleNb = i % TOTAL_RULE_NB;
+    }
+
+    private class RulePanel extends JPanel {
+        
+        private RulePanel() {
+            setLayout(new GridBagLayout());
+            setBackground(BACKGROUND);
+        }
+
+        private void addTextArea(int ruleNb) {
             GridBagConstraints elemGBC = new GridBagConstraints();
             elemGBC.gridx = 0;
             elemGBC.gridy = 1;
             elemGBC.anchor = GridBagConstraints.CENTER;
             elemGBC.fill = GridBagConstraints.BOTH;
             elemGBC.insets = new Insets(0, 30, 0, 30);
-            add(textArea, elemGBC);
+            //textAreas goes from 0 to 7, not 1 to 8
+            add(textAreas[ruleNb], elemGBC);
         }
 
-        private void addNextButton() {
+        private void addNextButton(int ruleNb) {
             JButton next = new RulesButton("Suivant");
             GridBagConstraints elemGBC = new GridBagConstraints();
             elemGBC.gridx = 0;
@@ -131,7 +167,8 @@ public class RulesPanel extends JPanel {
             elemGBC.weighty = .5;
             elemGBC.insets = new Insets(0, 0, 20, 20);
             next.addActionListener(buttonListener);
-            if (getRuleNb() == totalRuleNb) {
+            //ruleNb ranges from 0 to 7
+            if (ruleNb == TOTAL_RULE_NB - 1) {
                 next.setText("Terminer");
                 next.setActionCommand("endRule");
             } else {
@@ -140,8 +177,8 @@ public class RulesPanel extends JPanel {
             add(next, elemGBC);
         }
 
-        private void addPreviousButton() {
-            if (getRuleNb() != 1) {
+        private void addPreviousButton(int ruleNb) {
+            if (ruleNb != 0) {
                 JButton previous = new RulesButton("Précédent");
                 GridBagConstraints elemGBC = new GridBagConstraints();
                 elemGBC.gridx = 0;
@@ -153,26 +190,6 @@ public class RulesPanel extends JPanel {
                 previous.addActionListener(buttonListener);
                 previous.setActionCommand("previousRule");
                 add(previous, elemGBC);
-            }
-        }
-
-        private void ruleToShow() {
-            removeAll();
-            addTextArea("rule" + getRuleNb());
-            addNextButton();
-            addPreviousButton();
-        }
-
-        public int getRuleNb() {
-            return ruleNb;
-        }
-
-        public void setRuleNb(int i) {
-            if (i > 0 && i < totalRuleNb + 1) {
-                ruleNb = i;
-            } else {
-                Config.error("Can't assign invalid rule number. Valid range of rule number is 1 to " + totalRuleNb
-                        + " included");
             }
         }
     }
